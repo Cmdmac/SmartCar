1869d6cb73c8472156e12c0afb9ebed341f713bb
cmdmac <cmdmac@126.com>
2025-03-10 08:22:40 +0800

    add sr

---
+++ components/sr/Sr.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "esp_afe_sr_iface.h"
+#include "esp_afe_sr_models.h"
+#include "esp_wn_iface.h"
+#include "model_path.h"
+// #include "esp_wn_models.h
+#include "esp_mn_iface.h"
+#include "esp_mn_models.h"
+#include "esp_mn_speech_commands.h"
+
+#define ADC_I2S_CHANNEL 4
+
+class Sr {
+    public:
+        void setup();
+    
+    private:
+        void detect_Task(void *arg);
+        static void detectDelegate(void *pvParameters) {
+          Sr* instance = static_cast<Sr*>(pvParameters);
+          instance->detect_Task(instance->afe_data);
+        }
+
+        void feed_Task(void *arg);
+        static void feedDelegate(void *pvParameters) {
+          Sr* instance = static_cast<Sr*>(pvParameters);
+          instance->feed_Task(instance->afe_data);
+        }
+        srmodel_list_t *models = NULL;
+        esp_afe_sr_iface_t *afe_handle = NULL;
+        esp_afe_sr_data_t *afe_data = NULL;
+
+        int detect_flag = 0;
+        volatile int task_flag = 0;
+};
